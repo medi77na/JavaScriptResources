acef927535a73a3c46a6c6e7bb9f5b883b2dadc7
medi77na <medinastevenurregodavid@gmail.com>
2024-04-26 08:05:30 -0500

    Se crean ejemplos de funciones expresivas y declarativas

--- 09_funciones/src/js/main.js
+++ 09_funciones/src/js/main.js
@@ -1,13 +1,16 @@
-function recibirParametros(nombre, clan) {
-    return nombre, clan;
-}
+// // Funcion declarativa
+// function saludarCoder(nombre,clan) {
+//     console.log(`Hola ${nombre}, tu clan es: ${clan}.`);   
+// }
+// function despedirCoder(nombre) {
+//     console.log(`Hasta Luego ${nombre}, que estés muy bien!`);
+// }
 
-function saludarCoder(nombre,clan) {
-    // console.log(`Hola ${nombre}, tu clan es: ${clan}.`);   
-}
-function despedirCoder(nombre) {
-    // console.log(`Hasta Luego ${nombre}, que estés muy bien!`);
-}
+// saludarCoder("David","Gates");
+// despedirCoder("David")
 
-saludarCoder("David","Gates");
-despedirCoder("David")
+// //función expresiva
+// const holaMundo = function() {
+//     console.log("Hola Mundo");
+// }
+
